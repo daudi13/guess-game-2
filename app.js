595bc470f19f33f6645c44c67d26443589cec8ed
daudi13 <davouma13@gmail.com>
2021-10-11 11:17:14 +0300

    commit changes

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const score = document.querySelector('.score');
 const number = document.querySelector('.number');
 const data = document.getElementById('guess');
 
+const buttonCheck = document.querySelector('.check')
+
 
 message.textContent = 'correct Number!';
 score.textContent = 40;
 number.textContent = 2;
-data.value = 3
+
+buttonCheck.addEventListener('click', function () {
+    const guess = Number(document.getElementById('guess').value);
+    console.log(guess, typeof guess);
+    
+    if (!guess) {
+        message.textContent = 'enter input!!'
+    }
+})
