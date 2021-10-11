a42f8cf3e0a524350dbbf5e7284e1023276ea953
daudi13 <davouma13@gmail.com>
2021-10-11 10:43:03 +0300

    commit changes

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 const message = document.querySelector('.message');
 const score = document.querySelector('.score');
 const number = document.querySelector('.number');
+const data = document.getElementById('guess');
+
 
 message.textContent = 'correct Number!';
 score.textContent = 40;
-number.textContent = 22;
+number.textContent = 2;
+data.value = 3
