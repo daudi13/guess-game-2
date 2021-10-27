71b76ff6404c5b1325f219ef6b18c65f56d7bdc1
daudi13 <davouma13@gmail.com>
2021-10-27 22:23:46 +0300

    commit chngs

--- app.js
+++ app.js
@@ -15,45 +15,46 @@ let randomNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-
 btnCheck.addEventListener('click', function () {
-    const guess = Number(theInput.value);
     
+    const guess = Number(theInput.value);
+
     if (!guess) {
-        theMessage.innerHTML = 'No input'
+        theMessage.innerHTML = `NO NUMBER!!`;
     } else if (guess === randomNumber) {
+        theMessage.innerHTML = 'Great shot';
         theNumber.innerHTML = randomNumber;
-        body.style.backgroundColor = `#31f300`;
+        body.style.backgroundColor = '#f34544';
         theNumber.style.width = `30rem`;
         victory.play();
+
         if (score > highscore) {
-            highscore += score;
-            theHighscore.innerHTML = highscore;
+            highscore = score;
+            theHighscore.innerHTML = highscore
         }
     } else if (guess !== randomNumber) {
-        score--
-        theScore.innerHTML = score;
-        wrong.play();
-
-        if (guess > 1) {
+        if (score > 1) {
             guess > randomNumber ? theMessage.innerHTML = `too high` : theMessage.innerHTML = `too low`;
+            score--;
+            theScore.textContent = score;
+            wrong.play();
         } else {
-            theMessage.innerHTML = 'you lost try again';
-            theScore.innerHTML = 0;
+            theScore.textContent = 0;
+            messageDelivery(theMessage, 'you lost')
         }
     }
-
-    console.log(randomNumber);
 });
 
 btnAgain.addEventListener('click', function () {
     randomNumber = Math.trunc(Math.random() * 20) + 1;
     score = 20;
-    theMessage.innerHTML = `Start guessing`;
+    messageDelivery(theMessage, 'start guessing...');
     theScore.innerHTML = score;
-    body.style.backgroundColor = '#333';
-    theNumber.style.width = '15rem'
-    theNumber.innerHTML = '?';
-    theInput.value = '';
-    
+    body.style.backgroundColor = `#333`;
+    theNumber.style.width = '15rem';
+    messageDelivery(theNumber, '?')
 })
+
+function messageDelivery(element, message) {
+    return element.innerHTML = message;
+}
